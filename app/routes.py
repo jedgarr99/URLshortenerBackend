9d740f13b35a90ab0bf1e6702b4353c8f987f18e
unknown <jedgarr99@gmail.com>
2024-07-23 13:55:03 -0600

    10 commit

--- app/routes.py
+++ app/routes.py
@@ -38,7 +38,7 @@ def shorten_url():
     data = request.get_json()
     original_url = data['url']
 
-    prefix='http://localhost:5000/'
+    prefix='https://urlshortenerbackend-jaqc.onrender.com/'
     ending=''.join(random.choices(string.ascii_letters + string.digits, k=8))
     
     short_url = prefix+ending
