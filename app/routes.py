a227c13cdaf7d666cb8c59d5896b121357e8c89c
unknown <jedgarr99@gmail.com>
2024-07-23 14:46:38 -0600

    246 commit

--- app/routes.py
+++ app/routes.py
@@ -38,7 +38,7 @@ def shorten_url():
     data = request.get_json()
     original_url = data['url']
 
-    prefix='https://ur-lshortener-beryl.vercel.app/'
+    prefix='https://urlshortenerbackend-jaqc.onrender.com/'
     ending=''.join(random.choices(string.ascii_letters + string.digits, k=8))
     
     short_url = prefix+ending
