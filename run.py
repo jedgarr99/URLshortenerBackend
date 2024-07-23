31fd325625df3cf07eb920a44f2d1bce970b1e67
unknown <jedgarr99@gmail.com>
2024-07-23 13:40:51 -0600

    9 commit

---
+++ run.py
@@ -0,0 +1,10 @@
+from app import create_app,db
+from flask_migrate import Migrate
+
+app = create_app()
+migrate = Migrate(app, db)
+
+if __name__ == "__main__":
+   host='0.0.0.0'
+   print(host)
+   app.run(host='0.0.0.0', port=80)
